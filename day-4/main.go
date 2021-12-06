2db116fd79eecad84e22fa5dfba87c11024a37e9
Martijn-Faber <martijn@fabtronics.nl>
2021-12-06 20:34:57 +0100

    feat(day-4): refactor part 1 and add part 2

--- day-4/main.go
+++ day-4/main.go
@@ -1,58 +1,37 @@
 package main
 
 import (
-	"bufio"
+	"adventofcode/utils"
 	"fmt"
-	"os"
-	"strconv"
 	"strings"
 )
 
-var inputs []string
 const boardSize = 5
 
 type Board = [boardSize][boardSize]int
 
 func main() {
-	f, err := os.Open("input.txt")
-
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-
-	for scanner.Scan() {
-		t := scanner.Text()
-		inputs = append(inputs, t)
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println(err)
-	}
-
-	Part1()
+	// Part1()
+	Part2()
 }
 
 func Part1() {
+	input := utils.ReadInput("input.txt")
 	var boards []Board
 	var lastDrawnNumber int
 
-	numbers := strings.Split(inputs[0], ",")
+	numbers := strings.Split(input[0], ",")
 
 	// loop through the input data and append it to the 2d boards slice 
-	// start at index 2 because the first line are the numbers and the secund line is a space
-	// increase i by the boardsize and add the 1 for the space
-	for i := 2; i < len(inputs); i+=boardSize+1 {
+	// start at index 1 because the first line are the numbers
+	for i := 1; i < len(input); i+=boardSize {
 		var board = Board{}
 
 		for row := 0; row < boardSize; row++ {
-			line := strings.Fields(inputs[i + row])
+			line := strings.Fields(input[i + row])
 
 			for col, num := range line {
-				num, _ := strconv.ParseInt(num, 0, 64)
-				board[row][col] = int(num)
+				board[row][col] = utils.Atoi(num)
 			}
 		}
 
@@ -63,14 +42,12 @@ func Part1() {
 		for board := 0; board < len(boards); board++ {
 			for row := 0; row < boardSize; row++ {
 				for col := 0; col < boardSize; col++ {
-					num, _ := strconv.ParseInt(numbers[num], 0, 64)
-
-						if boards[board][row][col] == int(num) {
+						if boards[board][row][col] == utils.Atoi(numbers[num]) {
 							// set board index to negative 1
 							// these are marked
 							boards[board][row][col] = -1
 							// set last drawn number
-							lastDrawnNumber = int(num)
+							lastDrawnNumber = utils.Atoi(numbers[num])
 						}
 				}
 			}
@@ -128,6 +105,108 @@ func Part1() {
 
 			fmt.Printf("anwser part 1: %v", sum * lastDrawnNumber)
 			break
+		}
+	}
+}
+
+func Part2() {
+	input := utils.ReadInput("input.txt")
+	var boards []Board
+	var lastDrawnNumber int
+
+	numbers := strings.Split(input[0], ",")
+
+	// loop through the input data and append it to the 2d boards slice 
+	// start at index 1 because the first line are the numbers
+	for i := 1; i < len(input); i+=boardSize {
+		var board = Board{}
+
+		for row := 0; row < boardSize; row++ {
+			line := strings.Fields(input[i + row])
+
+			for col, num := range line {
+				board[row][col] = utils.Atoi(num)
+			}
+		}
+
+		boards = append(boards, board)
+	}
+
+	for num := 0; num < len(numbers); num++ {
+		for board := 0; board < len(boards); board++ {
+			for row := 0; row < boardSize; row++ {
+				for col := 0; col < boardSize; col++ {
+						if boards[board][row][col] == utils.Atoi(numbers[num]) {
+							// set board index to negative 1
+							// these are marked
+							boards[board][row][col] = -1
+							// set last drawn number
+							lastDrawnNumber = utils.Atoi(numbers[num])
+						}
+				}
+			}
+		}
+
+		// set bingo's default value to negative 1 so we can set the int to the board index
+		var bingo = -1
+		var remainingBoards []Board
+
+		for board := 0; board < len(boards); board++ {
+			bingo = -1
+			// check for bingo in rows
+			for row := 0; row < boardSize; row++ {
+				var count int
+				
+				for col := 0; col < boardSize; col++ {
+					if boards[board][row][col] == -1 {
+						count++
+					}
+
+					if count == boardSize {
+						// set bingo to the board index
+						bingo = board
+					}
+				}
+			}
+
+			// check for bingo in cols
+			for col := 0; col < boardSize; col++ {
+				var count int
+				for row := 0; row < boardSize; row++ {
+					if boards[board][row][col] == -1 {
+						count++
+					}
+				}
+
+				if count == boardSize {
+					// set bingo to the board index
+					bingo = board
+				}
+			}
+
+			if bingo < 0 {
+				remainingBoards = append(remainingBoards, boards[board])
+			}
+		}
+
+		// check if bingo is greater than 0 because we set bingo to the board index which can't be negative
+		if bingo >= 0 && len(boards) == 1 {
+			var sum int
+
+			// calculate the sum of the unmarked numbers
+			for col := 0; col < boardSize; col++ {
+				for row := 0; row < boardSize; row++ {
+					if boards[bingo][row][col] >= 0	{
+						sum += boards[bingo][row][col]
+					}
+				}
+			}
+
+			fmt.Printf("anwser part 2: %v", sum * lastDrawnNumber)
+			break
+		} else {
+			fmt.Println(len(remainingBoards))
+			boards = remainingBoards
 		}
 	}
 }
